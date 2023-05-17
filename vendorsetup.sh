0100b037d68a8b63ebb340026cf573d9ad1f4abc
Fahad Ali Javed <fahadalijaved@gmail.com>
2023-05-17 10:43:56 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_borag-user
-add_lunch_combo omni_borag-userdebug
-add_lunch_combo omni_borag-eng
+add_lunch_combo twrp_borag-user
+add_lunch_combo twrp_borag-userdebug
+add_lunch_combo twrp_borag-eng
